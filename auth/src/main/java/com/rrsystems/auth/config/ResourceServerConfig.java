c3e053aeef2a4e7584ff8af90cb2ff4683eac706
Rafael Reis <rafaelreis@Rafaels-MacBook-Air.local>
2020-12-06 17:42:09 -0300

    Atualizando diagrama e incluindo plano de testes.

--- auth/src/main/java/com/rrsystems/auth/config/ResourceServerConfig.java
+++ auth/src/main/java/com/rrsystems/auth/config/ResourceServerConfig.java
@@ -10,7 +10,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/").permitAll()
+			.antMatchers("/actuator/health").permitAll()
 			.anyRequest().authenticated();
 	}
 }
